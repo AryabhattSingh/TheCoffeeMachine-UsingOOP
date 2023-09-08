e0a818666754410239d499d48fc0cfa9e2ef5538
Aryabhatt <bhattsahab314@gmail.com>
2023-09-08 15:07:03 +0530

    Modified get_items() in menu.py. Instead of user typing the whole drink name, now user can just type 1, 2 or 3

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 class MenuItem:
     """Models each Menu Item."""
+
     def __init__(self, name, water, milk, coffee, cost):
         self.name = name
         self.cost = cost
@@ -12,6 +13,7 @@ class MenuItem:
 
 class Menu:
     """Models the Menu with drinks."""
+
     def __init__(self):
         self.menu = [
             MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
@@ -21,13 +23,16 @@ class Menu:
 
     def get_items(self):
         """Returns all the names of the available menu items"""
+        count = 1
         options = ""
         for item in self.menu:
-            options += f"{item.name}/"
+            options += f"Type {count} for {item.name}\n"
+            count += 1
         return options
 
     def find_drink(self, order_name):
-        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
+        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns
+        None"""
         for item in self.menu:
             if item.name == order_name:
                 return item
